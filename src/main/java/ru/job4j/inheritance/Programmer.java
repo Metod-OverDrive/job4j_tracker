ddb889ac59876ac626f707fd028b986e58dd4e80
Dmitry <pochta.nadomne@mail.ru>
2023-07-25 02:05:12 +0300

    3. Вызов конструктора родителя super(...) [#6837 # [#6837 #432115]]

--- src/main/java/ru/job4j/inheritance/Programmer.java
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class Programmer extends Engineer {
+
     private String programLang;
 
     public Programmer(String programLang, int experience, boolean degree) {
