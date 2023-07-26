5b9cdb197f5d85699bf074656e5aa72f5717290a
Dmitry <pochta.nadomne@mail.ru>
2023-07-26 18:18:05 +0300

    5. Tracker - хранилище [#396  [#396 #432394]]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,7 +18,7 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] result = new Item[100];
+        Item[] result = new Item[size];
         int resultSize = 0;
         for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
