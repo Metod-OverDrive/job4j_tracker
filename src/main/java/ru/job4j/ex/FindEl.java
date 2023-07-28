299fb17925f17ecfe1a7d1a115388f1ae9b132cd
Dmitry <pochta.nadomne@mail.ru>
2023-07-29 01:56:13 +0300

    0.5. Иерархия исключений и множественный catch. [#219368  [#219368]]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,26 +1,24 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        if (value[0] == null) {
-            throw new ElementNotFoundException("Array is null");
-        }
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (key.equals(value[i])) {
-                rsl = i;
-                break;
-            }
-        }
+        /* for-each */
         return rsl;
     }
 
-    public static void main(String[] args) {
-        String[] value = {"1", "2", "3", "4"};
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        /* if contains throw ElementAbuseException */
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
         try {
-            System.out.println(FindEl.indexOf(value, "2"));
-        } catch (ElementNotFoundException e) {
-            throw new RuntimeException(e);
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
