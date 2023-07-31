5a34a0ee37ba0a1ee799d77f5ed68403e3f7baf3
Dmitry <pochta.nadomne@mail.ru>
2023-07-31 23:38:55 +0300

    1.1. Тесты на StartUI. [#223313  [#223313]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -68,4 +68,24 @@ public class StartUITest {
                         + "0. Exit" + System.lineSeparator()
         );
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"15", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                "Menu." + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit" + ln
+        );
+    }
 }
